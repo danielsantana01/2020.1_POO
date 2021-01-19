3e7e5265bd229874667ebddc2d49e64a5a562450
Daniel Amaral de Santana <47670636+danielsantana01@users.noreply.github.com>
2021-01-19 16:39:49 -0300

    Update Program.cs

--- Lista_05/1035/Program.cs
+++ Lista_05/1035/Program.cs
@@ -5,7 +5,7 @@ namespace _1035 {
         static void Main(string[] args) {
             string[] e = Console.ReadLine().Split(' ');
             int a = int.Parse(e[0]), b = int.Parse(e[1]), c = int.Parse(e[2]), d = int.Parse(e[3]);
-            if(b>c && d>a && (c+d)>(a+b) && c>0 && d>0 && a%2 == 0) Console.WriteLine("Valores aceitos");
+            if(b>c && d>a && (c+d)>(a+b) && c>0 && d>0 && a%2==0) Console.WriteLine("Valores aceitos");
             else Console.WriteLine("Valores nao aceitos");
         }
     }
