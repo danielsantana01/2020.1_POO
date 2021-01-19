091f829fdbc059b38679dd8bfc5c725e6771b222
Daniel Amaral de Santana <47670636+danielsantana01@users.noreply.github.com>
2021-01-19 14:03:07 -0300

    Add files via upload

---
+++ 2456/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace POO {
+    class Program {
+        static void Main(string[] args) {
+            string[] e = Console.ReadLine().Split(' ');
+            int[] n = new int[5];
+            for(int i = 0; i < 5; i++) n[i] = int.Parse(e[i]);
+            int a = 0, b = 0;
+            bool C = true, D = true;
+            for(int i = 0; i < 4; i++){
+                a = n[i];
+                b = n[i + 1];
+                if(C && b < a) C = false;
+                if(D && b > a) D = false;
+                if(!(D) && !(C)) break;
+            }
+            if(C) Console.WriteLine("C");
+            else if(D) Console.WriteLine("D");
+            else Console.WriteLine("N");
+        }
+    }
+}
