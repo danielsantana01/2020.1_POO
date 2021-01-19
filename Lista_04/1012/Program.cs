d71044ba89fd25fc334d1d62718b554a03c39938
Daniel Amaral de Santana <47670636+danielsantana01@users.noreply.github.com>
2021-01-19 14:58:52 -0300

    Add files via upload

---
+++ Lista_04/1012/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] e = Console.ReadLine().Split(' ');
+            double a = double.Parse(e[0]), b = double.Parse(e[1]), c = double.Parse(e[2]);
+            Console.WriteLine($"TRIANGULO: {a*c/2:0.000}");
+            Console.WriteLine($"CIRCULO: {3.14159*Math.Pow(c,2):0.000}");
+            Console.WriteLine($"TRAPEZIO: {(a + b)*c/2:0.000}");
+            Console.WriteLine($"QUADRADO: {Math.Pow(b,2):0.000}");
+            Console.WriteLine($"RETANGULO: {a*b:0.000}");
+        }
+    }
+}
---
+++ Lista_04/1017/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace _1017 {
+    class Program {
+        static void Main(string[] args) {
+            double a = double.Parse(Console.ReadLine()), b = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{a*b/12:0.000}");
+        }
+    }
+}
